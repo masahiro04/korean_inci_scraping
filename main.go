eea7c07fbe19e51795ccc38affefaafe01dffa1a
masahirookubo <stmhamachiii@gmail.com>
2022-01-10 17:37:39 +0900

    tableのデータを取得する修正

--- main.go
+++ main.go
@@ -7,14 +7,25 @@ import (
 )
 
 func main() {
+	// var headings []string
+	var row []string
+	var rows [][]string
+
 	doc, err := goquery.NewDocument("https://kcia.or.kr/cid/search/ingd_list.php?page=1")
 	if err != nil {
 		fmt.Print("url scarappting faild")
 	}
 
-	doc.Find("tbody > tr").Each(func(_ int, s *goquery.Selection) {
-		fmt.Println("-------")
-		fmt.Println(s.Text())
-		fmt.Println("-------")
+	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
+		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
+			rowhtml.Find("td").Each(func(indextd int, cell *goquery.Selection) {
+				row = append(row, cell.Text())
+			})
+
+			rows = append(rows, row)
+			row = nil
+		})
 	})
+
+	fmt.Println(rows)
 }
