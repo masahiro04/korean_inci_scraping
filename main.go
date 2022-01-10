fca27e5d54b8ec2a143b1b09a23a70a448ba9a8c
masahirookubo <stmhamachiii@gmail.com>
2022-01-10 18:31:16 +0900

    structでまとめる修正

--- main.go
+++ main.go
@@ -6,10 +6,32 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type Row struct {
+	ID   string `csv:"id"`
+	Name string `csv:"名前"`
+	Inci string `csv:"INCI"`
+}
+
+func NewRow(id string, name string, inci string) *Row {
+	return &Row{
+		ID:   id,
+		Name: name,
+		Inci: inci,
+	}
+}
+
+func (row *Row) isValid() bool {
+	if &row.Name != nil || &row.Inci != nil {
+		return false
+	}
+
+	return true
+}
+
 func main() {
 	// var headings []string
-	var row []string
-	var rows [][]string
+	row := &Row{ID: "", Name: "", Inci: ""}
+	var rows []*Row
 
 	doc, err := goquery.NewDocument("https://kcia.or.kr/cid/search/ingd_list.php?page=1")
 	if err != nil {
@@ -19,13 +41,27 @@ func main() {
 	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
 		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
 			rowhtml.Find("td").Each(func(indextd int, cell *goquery.Selection) {
-				row = append(row, cell.Text())
+				// fmt.Println("index is ", indextd)
+				if indextd == 0 {
+					row.ID = cell.Text()
+				} else if indextd == 1 {
+					row.Name = cell.Text()
+				} else if indextd == 2 {
+					row.Inci = cell.Text()
+				}
+				// row = append(row, cell.Text())
 			})
+			fmt.Println(row)
 
-			rows = append(rows, row)
-			row = nil
+			if row.isValid() {
+				fmt.Println(row)
+				rows = append(rows, row)
+			}
+			// row = NewRow{id: "", name: "", inci: ""}
+			row = &Row{ID: "", Name: "", Inci: ""}
+
 		})
 	})
 
-	fmt.Println(rows)
+	fmt.Println(&rows)
 }
