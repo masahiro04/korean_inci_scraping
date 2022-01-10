9f33cce21af66993a6784b86d93f19725752e7d5
masahirookubo <stmhamachiii@gmail.com>
2022-01-10 18:58:53 +0900

    loop構文の追加

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocarina/gocsv"
@@ -24,7 +25,7 @@ type Row struct {
 
 func (row *Row) isValid() bool {
 	// NOTE(okubo): ルール決めておくべきかも
-	if &row.Name == nil || &row.Inci == nil {
+	if &row.Inci == nil {
 		return false
 	}
 
@@ -32,40 +33,41 @@ func (row *Row) isValid() bool {
 }
 
 func main() {
+	url := "https://kcia.or.kr/cid/search/ingd_list.php?page="
+
 	row := &Row{}
 	rows := []*Row{}
 
 	file, _ := os.OpenFile("koreandata.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
 	defer file.Close()
 
-	doc, err := goquery.NewDocument("https://kcia.or.kr/cid/search/ingd_list.php?page=1")
-	if err != nil {
-		fmt.Print("url scarappting faild")
+	for i := 0; i < 100; i++ {
+		fmt.Println(i)
+
+		doc, err := goquery.NewDocument(url + strconv.Itoa(i))
+		if err != nil {
+			fmt.Print("url scarappting faild")
+		}
+
+		doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
+			tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
+				rowhtml.Find("td").Each(func(indextd int, cell *goquery.Selection) {
+					if indextd == 0 {
+						row.ID = cell.Text()
+					} else if indextd == 1 {
+						row.Name = cell.Text()
+					} else if indextd == 2 {
+						row.Inci = cell.Text()
+					}
+				})
+
+				if row.isValid() {
+					rows = append(rows, row)
+				}
+				row = &Row{ID: "", Name: "", Inci: ""}
+			})
+		})
 	}
 
-	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
-		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
-			rowhtml.Find("td").Each(func(indextd int, cell *goquery.Selection) {
-				// fmt.Println("index is ", indextd)
-				if indextd == 0 {
-					row.ID = cell.Text()
-				} else if indextd == 1 {
-					row.Name = cell.Text()
-				} else if indextd == 2 {
-					row.Inci = cell.Text()
-				}
-				// row = append(row, cell.Text())
-			})
-
-			if row.isValid() {
-				fmt.Println(row)
-				rows = append(rows, row)
-			}
-			row = &Row{ID: "", Name: "", Inci: ""}
-
-		})
-	})
-
 	gocsv.MarshalFile(&rows, file)
-	fmt.Println(&rows)
 }
